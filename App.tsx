35f6e4f5c25fb070a314cb4289d7b909c2434739
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-12-12 13:54:11 -0200

    Update project structure

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import { IAppState } from "./IAppState";
 import { TaskForm } from "./todo/form/TaskForm";
 import { ITask } from "./todo/ITask";
-import { TaskListItem } from "./todo/TaskListItem";
+import { ListItem } from "./todo/list-item/ListItem";
 
 export default class App extends React.Component<{}, IAppState> {
 
@@ -25,7 +25,7 @@ export default class App extends React.Component<{}, IAppState> {
     const currentTaskItems = this.state.taskItems;
     this.setState({ taskItems: currentTaskItems.concat(task) });
   }
-  private readonly renderItem = (task) => <TaskListItem task={task} />;
+  private readonly renderItem = (task) => <ListItem task={task} />;
 }
 
 const styles = StyleSheet.create({
